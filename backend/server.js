fedd084fd723723882ec0ef0f4448ec1f2001111
Het Vashi <hetvashi1052@gmail.com>
2022-01-06 08:40:26 +0530

    change in server js.

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ app.use(express.json())
 const db = "mongodb://localhost:27017/todo"
 
 mongoose.connect(db, ({ useNewUrlParser: true }))
-    .then(console.log("Connected to mongo"))
+    .then(console.log("Connected to mongo database"))
     .catch(err => console.log(err))
 
 const todoSchema = new mongoose.Schema({
     title: String,
     complete: {
-        type: Number,
-        default: 0
+        type: boolean,
+        default: false
     },
     Date: {
         type: Date,
